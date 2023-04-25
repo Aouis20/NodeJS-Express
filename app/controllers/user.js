ecba15cc50277d8d5f2cfc31c95ab3818d57cec2
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-04-25 14:08:03 +0200

    Signup and Login controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,12 +1,35 @@
-const User = require("../models/user")
+const bcrypt = require('bcrypt');
+const {
+    User
+} = require("../models");
 
 exports.signup = async (req, res) => {
+    const {
+        firstname,
+        lastname,
+        email,
+        password
+    } = req.body
+
     try {
+        const existingUser = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
+
+        if (existingUser) {
+            return res.status(401).json({
+                error: 'User already exists',
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
-            firstName: req.body.firstname,
-            lastName: req.body.lastname,
-            email: req.body.email,
-            password: req.body.password
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            password: hashedPassword
         });
         res.status(201).json(user);
     } catch (error) {
@@ -16,7 +39,41 @@ exports.signup = async (req, res) => {
     }
 };
 
-exports.login = (req, res) => {
-    res.send('You are logged in');
-    res.json(req.body)
-}
+exports.login = async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+
+        const user = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                error: 'Utilisateur introuvable',
+            });
+        }
+
+        // Vérification du mot de passe
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                error: 'Mot de passe incorrect',
+            });
+        }
+
+        res.status(200).json({
+            error: 'Connexion réussi'
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+};
