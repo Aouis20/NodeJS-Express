cef336ec26d5c8465525820fa47071d65ec851a2
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-04-26 15:08:07 +0200

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = require("./app/routes/index.js");
 const app = express()
 const path = require('path')
+const cors = require('cors')
+
 
 const db = require("./app/models/index.js");
 db.sequelize
@@ -9,12 +11,12 @@ db.sequelize
     .then(() => console.log("Database connected ..."))
     .catch((err) => console.log(err));
 
-
-//Ajout des routes
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+
+//Ajout des routes
 app.use("/api", router);
 
 // Permet de servir statiquement les assets
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 require('dotenv').config()
 const app = require("./app.js")
 const port = process.env.PORT
+const cors = require('cors')
+
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200
+}
+// Pour tester les cors il faut absolument un front
+
+app.use(cors(corsOptions))
 
 app.get((req, res) => {
     res.send('Hello World ! (server.js)')
