57e5b2eb26dae2139d3ab21fcb14562408f69113
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-04-27 16:31:13 +0200

    Update wood.js

--- app/models/wood.js
+++ app/models/wood.js
@@ -20,28 +20,6 @@ module.exports = (sequelize, DataTypes) => {
   }
   Wood.init({
     name: DataTypes.STRING,
-    type: {
-      type: DataTypes.ENUM('softwood', 'exotic wood', 'noble and hardwoods'),
-      validate: {
-        isIn: {
-          args: [
-            ['softwood', 'exotic wood', 'noble and hardwoods']
-          ],
-          msg: 'Le champ type doit être soit softwood, exotic wood ou noble and hardwoods.'
-        }
-      }
-    },
-    hardness: {
-      type: DataTypes.ENUM('tender', 'medium-hard', 'hard'),
-      validate: {
-        isIn: {
-          args: [
-            ['tender', 'medium-hard', 'hard']
-          ],
-          msg: 'Le champ hardness doit être soit tender, medium-hard ou hard.'
-        }
-      }
-    },
     image: DataTypes.STRING,
   }, {
     sequelize,
